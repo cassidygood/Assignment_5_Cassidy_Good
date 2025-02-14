d0c473a877ca6c818d447ed8c2b84683e5799b51
cassidygood <cassidy.r.good-1@ou.edu>
2025-02-13 23:27:39 -0800

    Added a new script and changed the coin script.
    Added a new (empty) script for the heart beat. Created a heart. Also modified the script for the coin pickup so the coins have different values.

--- Assets/Scripts/CG_CoinPickup.cs
+++ Assets/Scripts/CG_CoinPickup.cs
@@ -2,7 +2,27 @@ using UnityEngine;
 
 public class CG_CoinPickup : MonoBehaviour
 {
-    public int points = 10; // Points given for collecting this coin
+    private int points = 10; // Default points
+
+    private void Start()
+{
+    switch (gameObject.tag)
+    {
+        case "RareCoin":
+            points = 50;
+            break;
+        case "MidCoin":
+            points = 30;
+            break;
+        case "LowCoin":
+            points = 10;
+            break;
+        default:
+            points = 5;
+            break;
+    }
+}
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -11,10 +31,11 @@ public class CG_CoinPickup : MonoBehaviour
             PlayerScore playerScore = other.GetComponent<PlayerScore>();
             if (playerScore != null)
             {
-                playerScore.AddScore(points); // Add points to the player's score
+                playerScore.AddScore(points); // Add points based on coin type
             }
 
             Destroy(gameObject); // Remove the coin after pickup
         }
     }
 }
+
