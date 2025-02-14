1123ab09f0d509b3fd42815af7e2beff758eb2e6
cassidygood <cassidy.r.good-1@ou.edu>
2025-02-14 00:57:57 -0800

    Changed and then deleted coin pickup script

--- Assets/Scripts/CG_CoinPickup.cs
+++ Assets/Scripts/CG_CoinPickup.cs
@@ -5,24 +5,24 @@ public class CG_CoinPickup : MonoBehaviour
     private int points = 10; // Default points
 
     private void Start()
-{
-    switch (gameObject.tag)
     {
-        case "RareCoin":
-            points = 50;
-            break;
-        case "MidCoin":
-            points = 30;
-            break;
-        case "LowCoin":
-            points = 10;
-            break;
-        default:
-            points = 5;
-            break;
+        // Assign different scores based on the coin's name
+        switch (gameObject.name)
+        {
+            case "Ruby":
+                points = 50;
+                break;
+            case "Gold":
+                points = 30;
+                break;
+            case "Moonstone":
+                points = 10;
+                break;
+            default:
+                points = 5; // Default value for unnamed or other coins
+                break;
+        }
     }
-}
-
 
     private void OnTriggerEnter(Collider other)
     {
@@ -31,11 +31,13 @@ public class CG_CoinPickup : MonoBehaviour
             PlayerScore playerScore = other.GetComponent<PlayerScore>();
             if (playerScore != null)
             {
-                playerScore.AddScore(points); // Add points based on coin type
+                playerScore.AddScore(points); // Add points to the player's score
             }
+
+            // Print the coin value to the Console
+            Debug.Log("Collected: " + gameObject.name + " | Value: " + points);
 
             Destroy(gameObject); // Remove the coin after pickup
         }
     }
 }
-
